41602c4da7285878bcdd17626d6740a11a02cc48
komatsu1173 <yu85wave@gmail.com>
2026-01-09 17:24:46 +0900

    fix:ReturnMapper.java 返却用Mapper 更新後にシリアルナンバーを生成し貸出可能に

--- equipment/src/main/java/jp/co/sss/equipment/mapper/ReturnMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/ReturnMapper.java
@@ -15,9 +15,12 @@ public interface ReturnMapper {
 	//返却画面時に表示させる(貸出中のみ)
 	List<DetailListViewDto> returnFind(@Param("name") String name);
 
-//	//返却の更新 stock_master
+//	//返却の更新 stock_master （故障時）
 //	int rentFlagUpdate(List<String> stockCodes);
 	
 	//返却時の更新　stock_data
 	void stockDataUpdate(List<Integer> stockCodes);
+	
+	//貸出可能のする 返却されたシリアルナンバーを新しくid生成
+	void insertRebornStock(Integer id);
 }
