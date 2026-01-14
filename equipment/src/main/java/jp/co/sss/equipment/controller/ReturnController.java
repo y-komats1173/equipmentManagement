dcf55cc2039a2df1a84725344989bb611223f6f2
komatsu1173 <yu85wave@gmail.com>
2026-01-14 11:47:29 +0900

    fix:ReturnController データが取得できているかの確認処理を追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/ReturnController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/ReturnController.java
@@ -38,6 +38,24 @@ public class ReturnController {
 		List<DetailListViewDto> detailName = indexService.detailFind(name);//貸出中の備品を取得する
 		model.addAttribute("detailName", detailName.get(0));//備品名をひとつ取得し、HTMLに表示させる
 		model.addAttribute("itemDetail", returnList);//貸出中の備品をHTMLのテーブルに表示させる
+
+		//デバッグ
+		int num = 0;
+		for (DetailListViewDto i : detailName) {
+			num++;
+			System.out.println("===========" + num + "===========");
+			System.out.println("===========" + "返却" + "===========");
+			System.out.println("備品名:" + i.getEquipmentName());
+			System.out.println("シリアル:" + i.getParentStockCode());
+			System.out.println("使用者:" + i.getStaffName());
+			System.out.println("貸出可否:" + i.getRentFlg());
+			System.out.println("貸出開始日:" + i.getStartDate());
+			System.out.println("返却予定日:" + i.getLimitDate());
+			System.out.println("最終所在確認" + i.getConfirmedDate());
+			System.out.println("備考:" + i.getRemarks());
+			System.out.println("シーケンスID: " + i.getEquipmentId());
+			System.out.println("スタッフID:" + i.getStaffNo());
+		}
 		return "return/returnView"; //templatesフォルダーのhtmlを表示させる
 	}
 
@@ -46,11 +64,11 @@ public class ReturnController {
 	 */
 	@PostMapping("/returnProcess")
 	public String returnProcess(
-			@RequestParam(value = "equipmentIdList", required = false)List<Integer> equipmentIdList,//Listにidが格納されている
+			@RequestParam(value = "equipmentIdList", required = false) List<Integer> equipmentIdList, //Listにidが格納されている
 			@RequestParam("name") String name,
 			Model model) {
-		if(equipmentIdList != null && !equipmentIdList.isEmpty()) {  //チェックが入っている場合
-		returnService.returnEquipment(equipmentIdList); //サービス層でsqlのマッパー呼び出し
+		if (equipmentIdList != null && !equipmentIdList.isEmpty()) { //チェックが入っている場合
+			returnService.returnEquipment(equipmentIdList); //サービス層でsqlのマッパー呼び出し
 		}
 		System.out.println("equipmentIdList = " + equipmentIdList);
 		List<DetailListViewDto> returnViewList = returnService.returnFindView(name); //備品名を取得する　サービス層で処理
