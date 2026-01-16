95de1e4675f39ecb3e38bc51dbea2306449125c3
komatsu1173 <yu85wave@gmail.com>
2026-01-16 17:22:53 +0900

    fix: BorrowingService.java ListからMapの処理に修正

--- equipment/src/main/java/jp/co/sss/equipment/service/BorrowingService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/BorrowingService.java
@@ -2,6 +2,7 @@ package jp.co.sss.equipment.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,22 +41,67 @@ public class BorrowingService {
 	/**
 	 * 貸出更新
 	 */
+	//	@Transactional
+	//	public void borrowingEquipment(
+	//	    List<Integer> equipmentIdList,
+	//	    List<Integer> staffNoList,
+	//	    List<LocalDate> startDateList,
+	//	    List<LocalDate> limitDateList
+	//	) {
+	//	    for (int i = 0; i < equipmentIdList.size(); i++) {
+	//
+	//	        LocalDate start = startDateList.get(i);
+	//	        LocalDate limit = limitDateList.get(i);
+	//
+	//	        // 未入力は処理しない
+	//	        if (start == null || limit == null) {
+	//	            continue;
+	//	        }
+	//
+	//	        // 開始日 <= 返却日
+	//	        if (!start.isAfter(limit)) {
+	//	            borrowingMapper.borrowingUpdate(
+	//	                equipmentIdList.get(i),
+	//	                staffNoList.get(i),
+	//	                start,
+	//	                limit
+	//	            );
+	//	        }
+	//	    }
+	//	}
+
 	@Transactional
 	public void borrowingEquipment(
-	    List<Integer> equipmentIdList,
-	    List<Integer> staffNoList,
-	    List<LocalDate> startDateList,
-	    List<LocalDate> limitDateList //コントローラからの引数(HTMLからのデータ)
-	) {
-	    for (int i = 0; i < equipmentIdList.size(); i++) {//チェックの数だけ繰り返す
-	    	if(startDateList.get(i).isBefore(limitDateList.get(i))) {//貸出開始日が返却日より前の日付か確認
-	        borrowingMapper.borrowingUpdate(//Mapperを呼び出す
-	            equipmentIdList.get(i), //id
-	            staffNoList.get(i),//社員id
-	            startDateList.get(i),//貸出開始日
-	            limitDateList.get(i)//貸出期限
-	        );  //Listのi番目を一斉に渡しマッパーで処理をする
-	    }
+	        List<Integer> equipmentIdList,
+	        Map<String, String> staffNoMap,
+	        Map<String, String> startDateMap,
+	        Map<String, String> limitDateMap) {
+
+	    for (Integer id : equipmentIdList) {
+
+	        String staffKey = "staffNoMap[" + id + "]";
+	        String startKey = "startDateMap[" + id + "]";
+	        String limitKey = "limitDateMap[" + id + "]";
+
+	        String staffStr = staffNoMap.get(staffKey);
+	        String startStr = startDateMap.get(startKey);
+	        String limitStr = limitDateMap.get(limitKey);
+
+	        if (staffStr == null || startStr == null || limitStr == null) {
+	            continue;
+	        }
+
+	        LocalDate start = LocalDate.parse(startStr);
+	        LocalDate limit = LocalDate.parse(limitStr);
+
+	        if (!start.isAfter(limit)) {
+	            borrowingMapper.borrowingUpdate(
+	                    id,
+	                    Integer.valueOf(staffStr),
+	                    start,
+	                    limit
+	            );
+	        }
 	    }
 	}
 }
