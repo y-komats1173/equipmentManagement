79ce6e1a0311068ea09fb7347b1bc738caf9f009
komatsu1173 <yu85wave@gmail.com>
2026-01-29 10:30:26 +0900

    feat: IndexControllerに備品詳細情報のログ出力を追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
@@ -46,6 +46,28 @@ public class IndexController {
 		List<DetailListViewDto> detailList = indexService.detailFind(name);//サービス層のdetailFindメソッドを呼び出し値をリストに返す
 		model.addAttribute("detailName", detailList.get(0)); //備品名が複数取得されるため１つ目だけ採取しHTMLに反映（１つだけのため）
 		model.addAttribute("itemDetail", detailList); 
+		
+		//値取得チェック
+		for (DetailListViewDto dto : detailList) {
+			System.out.println("備品名：" + dto.getEquipmentName());
+			System.out.println("カテゴリ番号：" + dto.getStockType());
+			System.out.println("シリアルナンバー：" + dto.getParentStockCode());
+			System.out.println("使用者：" + dto.getStaffName());
+			System.out.println("貸出可否：" + dto.getRentFlg());
+			System.out.println("貸出開始日：" + dto.getStartDate());
+			System.out.println("返却予定日：" + dto.getLimitDate());
+			System.out.println("最終所在確認：" + dto.getConfirmedDate());
+			System.out.println("親シリアル：" + dto.getDestinationSerial());
+			System.out.println("製品名：" + dto.getProductName());
+			System.out.println("メーカー名：" + dto.getMaker());
+			System.out.println("分類：" + dto.getOwnershipType());
+			System.out.println("リース返却予定日：" + dto.getLeaseReturnDate());
+			System.out.println("備考：" + dto.getRemarks());
+			System.out.println("シーケンスid：" + dto.getEquipmentId());
+			System.out.println("スタッフid：" + dto.getStaffNo());
+			System.out.println("---------------------------");
+		}
+		
 		return "index/detail";
 	}
 	
