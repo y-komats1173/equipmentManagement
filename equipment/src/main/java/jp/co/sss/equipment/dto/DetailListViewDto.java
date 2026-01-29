fbdda3a30ba4bc90db95bc9b72fd0e488b180a29
komatsu1173 <yu85wave@gmail.com>
2026-01-29 10:30:21 +0900

    feat: DetailListViewDtoに新しいプロパティを追加

--- equipment/src/main/java/jp/co/sss/equipment/dto/DetailListViewDto.java
+++ equipment/src/main/java/jp/co/sss/equipment/dto/DetailListViewDto.java
@@ -2,10 +2,19 @@ package jp.co.sss.equipment.dto;
 
 import lombok.Data;
 
+/**
+ * 備品詳細一覧画面DTO
+ * 
+ * @author yuhakomatsubara
+ */
 @Data
 public class DetailListViewDto {
 	/**備品名*/
 	private String equipmentName;
+	
+	/**追加機能
+	 * カテゴリ番号 */
+	private String stockType;
 	
 	/**シリアルナンバー*/
 	private String parentStockCode;
@@ -24,6 +33,26 @@ public class DetailListViewDto {
 	
 	/**最終所在確認*/
 	private String confirmedDate;
+	
+	/**追加機能
+	 * 親シリアル（接続先PCシリアルナンバー）*/
+	private String destinationSerial;
+	
+	/**追加機能
+	 * 製品名*/
+	private String productName;
+	
+	/**追加機能
+	 * メーカー名*/
+	 private String maker;
+	 
+	 /**追加機能
+	  *分類（社内保有・リース品）*/
+	 private String ownershipType;
+	 
+	 /**追加機能
+	  * リース返却予定日*/
+	 private String leaseReturnDate;
 	
 	/**備考*/
 	private String remarks;
