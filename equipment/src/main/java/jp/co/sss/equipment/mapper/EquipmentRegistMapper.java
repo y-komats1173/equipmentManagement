a7ae48a0a009bca56bb355c00428f984993d73a0
komatsu1173 <yu85wave@gmail.com>
2026-02-05 17:58:50 +0900

    add: EquipmentRegistMapperに新しいinsertメソッドとupdateメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/mapper/EquipmentRegistMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/EquipmentRegistMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.query.Param;
 
+import jp.co.sss.equipment.entity.StockData;
 import jp.co.sss.equipment.entity.StockMaster;
 import jp.co.sss.equipment.entity.StockTypeMaster;
 
@@ -30,8 +31,27 @@ public interface EquipmentRegistMapper {
 	StockTypeMaster findByCategoryId(@Param("categoryId") Integer categoryId);
 
 	/**
+	 * 備品登録挿入（idの追加）
+	 */
+	void equipmentRegistInsert(StockMaster stockMaster);	
+	
+	/**
 	 * 備品登録挿入
 	 * @param stockMaster
 	 */
-	void equipmentRegistInsert(StockMaster stockMaster);
+	void equipmentRegistUpdate(StockMaster stockMaster);
+
+	/**
+	 * 貸出開始処理挿入
+	 * @param stockCode
+	 */
+	void insertForId(StockData stockData);
+	
+	/**
+	 * 備品貸出開始挿入
+	 * @param stockData
+	 */
+	void equimentBorrowingStartInsert(StockData stockData);
+
+
 }
