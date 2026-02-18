0f319dde7d8a5e7b06ec5d3c17820a59ca6c8ff8
komatsu1173 <yu85wave@gmail.com>
2026-02-18 17:30:12 +0900

    add:EquipmentCommonMapper.java 登録・編集用の共通マッパーを追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/EquipmentCommonMapper.java
@@ -0,0 +1,27 @@
+package jp.co.sss.equipment.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.repository.query.Param;
+
+import jp.co.sss.equipment.entity.StockTypeMaster;
+
+/**
+ * 備品登録・備品編集共通マッパー
+ */
+@Mapper
+public interface EquipmentCommonMapper {
+	/**
+	 * カテゴリ取得
+	 * @return
+	 */
+	List<StockTypeMaster> categoryFind();
+	
+	/**
+	 * カテゴリIDからカテゴリ情報を取得
+	 * @param categoryId
+	 * @return
+	 */
+	StockTypeMaster findByCategoryId(@Param("categoryId") Integer categoryId);
+}
