036144c3701b71fe58784624ed642cbe70b660c2
komatsu1173 <yu85wave@gmail.com>
2026-02-27 15:26:09 +0900

    fix:LoginUserInterceptor.java コメント追加

--- equipment/src/main/java/jp/co/sss/equipment/config/LoginUserInterceptor.java
+++ equipment/src/main/java/jp/co/sss/equipment/config/LoginUserInterceptor.java
@@ -16,9 +16,11 @@ import jp.co.sss.equipment.filter.LoginCheckFilter;
 public class LoginUserInterceptor implements HandlerInterceptor {
 
 	@Override
+	// コントローラーの前に実行されるメソッド
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 	    HttpSession session = request.getSession();
 	    Object user = session.getAttribute("user");
+	    // ログインユーザーが存在する場合、リクエスト属性に設定
 	    if (user != null) {
 	        request.setAttribute("loginUser", user);
 	    }
@@ -26,10 +28,13 @@ public class LoginUserInterceptor implements HandlerInterceptor {
 	}
 	
 	@Bean
+	// フィルタの登録
 	public FilterRegistrationBean<LoginCheckFilter> loginCheckFilter() {
+		// FilterRegistrationBeanを作成し、LoginCheckFilterを登録
 	    FilterRegistrationBean<LoginCheckFilter> bean =
 	            new FilterRegistrationBean<>();
 
+	    // LoginCheckFilterをフィルタとして設定
 	    bean.setFilter(new LoginCheckFilter());
 	    bean.addUrlPatterns("/*"); // 全部対象
 	    bean.setOrder(1); 
@@ -37,8 +42,11 @@ public class LoginUserInterceptor implements HandlerInterceptor {
 	}
 	
 	@Bean
+	// フィルタの登録
 	public FilterRegistrationBean<AuthorityCheckFilter> authorityCheckFilter() {
+		// FilterRegistrationBeanを作成し、AuthorityCheckFilterを登録
 	    FilterRegistrationBean<AuthorityCheckFilter> bean = new FilterRegistrationBean<>();
+	    // AuthorityCheckFilterをフィルタとして設定
 	    bean.setFilter(new AuthorityCheckFilter());
 	    bean.addUrlPatterns("/*");
 	    bean.setOrder(2); // LoginCheckFilter(1)の後
