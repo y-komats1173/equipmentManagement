74459c970453e0e25e718c88f62b64ad0e32e1cf
komatsu1173 <yu85wave@gmail.com>
2026-03-03 13:59:20 +0900

    add:BorrowingHistoryService.java 貸出履歴表示用のサービスクラスを追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/service/BorrowingHistoryService.java
@@ -0,0 +1,27 @@
+package jp.co.sss.equipment.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jp.co.sss.equipment.dto.BorrowingHistoryDto;
+import jp.co.sss.equipment.mapper.BorrowingHistoryMapper;
+
+/**
+ * 貸出履歴を操作するサービス
+ */
+@Service
+public class BorrowingHistoryService {
+    @Autowired
+    BorrowingHistoryMapper borrowingHistoryMapper;
+
+    /**
+     * 貸出履歴を表示する
+     * @return
+     */
+    public List<BorrowingHistoryDto> findBorrowingHistory() {
+        return borrowingHistoryMapper.findBorrowingHistory();
+    }
+
+}
