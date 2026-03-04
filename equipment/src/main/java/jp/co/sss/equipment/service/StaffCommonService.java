4b9c3a1d4dff5e8b9fb0181d61276924d9e3ff99
komatsu1173 <yu85wave@gmail.com>
2026-03-04 15:34:31 +0900

    fix:StaffCommonService.java 社員検索用のサービスを作成

---
+++ equipment/src/main/java/jp/co/sss/equipment/service/StaffCommonService.java
@@ -0,0 +1,26 @@
+package jp.co.sss.equipment.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jp.co.sss.equipment.entity.StaffData;
+import jp.co.sss.equipment.mapper.StaffCommonMapper;
+
+/**
+ *社員共通サービス
+ */
+@Service
+public class StaffCommonService {
+	@Autowired
+	private StaffCommonMapper staffCommonMapper;
+	
+	/**
+	 * スタッフデータを取得
+	 * @return
+	 */
+	public List<StaffData> staffDataFind() {
+		return staffCommonMapper.staffFind();//マッパーの処理を返す
+	}
+}
