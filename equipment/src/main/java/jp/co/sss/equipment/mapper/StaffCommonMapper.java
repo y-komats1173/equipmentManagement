1e6d44e1e7f154c025b911effbcb19b89a290ee6
komatsu1173 <yu85wave@gmail.com>
2026-03-17 10:15:41 +0900

    fix:StaffCommonMapper.java　権限取得のマッパーメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/mapper/StaffCommonMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/StaffCommonMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import jp.co.sss.equipment.entity.AuthMaster;
 import jp.co.sss.equipment.entity.StaffData;
 
 /**
@@ -16,9 +17,21 @@ public interface StaffCommonMapper {
 	 * 使用者・社員名の取得
 	 */
 	List<StaffData> staffFind();
-	
+
 	/*
 	 * 個別詳細検索
 	 */
 	StaffData staffFindIndividual(Integer staffNo);
+
+	/**
+	 * 権限取得
+	 * @return
+	 */
+	List<AuthMaster> authFind();
+
+	/**
+	 * 権限の取得１つのカテゴリ
+	 * @return
+	 */
+	AuthMaster authFindById(Integer authNo);
 }
