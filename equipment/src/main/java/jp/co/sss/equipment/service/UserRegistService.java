61999d81d8f0852b7ed0828238a608a3bf230562
komatsu1173 <yu85wave@gmail.com>
2026-03-18 16:08:08 +0900

    add:UserRegistService.java ユーザー登録時の処理・ID重複チェックの追加

--- equipment/src/main/java/jp/co/sss/equipment/service/UserRegistService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/UserRegistService.java
@@ -1,11 +1,14 @@
 package jp.co.sss.equipment.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.form.UserRegistForm;
+import jp.co.sss.equipment.mapper.StaffCommonMapper;
 import jp.co.sss.equipment.mapper.UserRegistMapper;
 import jp.co.sss.equipment.util.BeanCopy;
 
@@ -17,6 +20,9 @@ public class UserRegistService {
 	@Autowired
 	UserRegistMapper userRegistMapper;
 	
+	@Autowired
+	StaffCommonMapper staffCommonMapper;
+	
 	/**
 	 *ユーザー登録処理
 	 */
@@ -27,7 +33,21 @@ public class UserRegistService {
 		//論理削除フラグを0に
 		staffData.setDel("0");
 		//登録
-		userRegistMapper.userRegistInsert(staffData);
-		
+		userRegistMapper.userRegistInsert(staffData);		
+	}
+
+	/**
+	 * IDの重複チェック
+	 * @param staffNo
+	 * @return
+	 */
+	public boolean idCheck(Integer staffNo) {
+		List<StaffData> staffData = staffCommonMapper.staffFind();
+		 for (StaffData staff : staffData) {
+		        if (staff.getStaffNo().equals(staffNo)) {
+		            return true; // 重複あり
+		        }
+		    }
+		    return false; // 重複なし
 	}
 }
