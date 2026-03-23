dc15e47d01a6ba6dc144901460f7b2d7797f7be7
komatsu1173 <yu85wave@gmail.com>
2026-03-23 13:22:42 +0900

    fix:BeanCopy.java UserFormクラス名変更による引数の変更

--- equipment/src/main/java/jp/co/sss/equipment/util/BeanCopy.java
+++ equipment/src/main/java/jp/co/sss/equipment/util/BeanCopy.java
@@ -3,7 +3,7 @@ package jp.co.sss.equipment.util;
 import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.entity.StockMaster;
 import jp.co.sss.equipment.form.EquipmentForm;
-import jp.co.sss.equipment.form.UserRegistForm;
+import jp.co.sss.equipment.form.UserForm;
 
 /**
  * Beanコピー用クラス
@@ -21,7 +21,7 @@ public class BeanCopy {
 	 * @return エンティティ
 	 */
 	public static StockMaster copyFormToStockMaster(EquipmentForm equipmentRegistForm) {
-		StockMaster entity = new StockMaster();	
+		StockMaster entity = new StockMaster();
 		entity.setStockType(equipmentRegistForm.getCategoryId());
 		entity.setName(equipmentRegistForm.getEquipmentName());
 		entity.setModel(equipmentRegistForm.getModel());
@@ -29,10 +29,10 @@ public class BeanCopy {
 		entity.setRentFlag(equipmentRegistForm.getRentFlag());
 		entity.setOwnershipType(equipmentRegistForm.getOwnershipType());
 		entity.setLeaseReturnDate(equipmentRegistForm.getLeaseReturnDate());
-		entity.setRemarks(equipmentRegistForm.getRemarks());		
+		entity.setRemarks(equipmentRegistForm.getRemarks());
 		return entity;
 	}
-	
+
 	/**
 	 * Formクラスの各フィールドの値をエンティティ(EquipmentUpdateForm)にコピー
 	 *
@@ -43,24 +43,24 @@ public class BeanCopy {
 	public static EquipmentForm copyDtoToUpdateForm(StockMaster stockMaster) {
 		EquipmentForm form = new EquipmentForm();
 
-	    form.setCategoryId(stockMaster.getStockType());
-	    form.setEquipmentName(stockMaster.getName());
-	    form.setModel(stockMaster.getModel());
-	    form.setMaker(stockMaster.getMaker());
-	    form.setOwnershipType(stockMaster.getOwnershipType());
-	    form.setLeaseReturnDate(stockMaster.getLeaseReturnDate());
-	    form.setRemarks(stockMaster.getRemarks());
-	    form.setRentFlag(stockMaster.getRentFlag());
-	    form.setStockCode(stockMaster.getStockCode());
+		form.setCategoryId(stockMaster.getStockType());
+		form.setEquipmentName(stockMaster.getName());
+		form.setModel(stockMaster.getModel());
+		form.setMaker(stockMaster.getMaker());
+		form.setOwnershipType(stockMaster.getOwnershipType());
+		form.setLeaseReturnDate(stockMaster.getLeaseReturnDate());
+		form.setRemarks(stockMaster.getRemarks());
+		form.setRentFlag(stockMaster.getRentFlag());
+		form.setStockCode(stockMaster.getStockCode());
 
-	    return form;
+		return form;
 	}
-	
+
 	/**
 	 * ユーザー情報
 	 * Formクラスからentityに値をコピー
 	 */
-	public static StaffData userCopyEntity (UserRegistForm registform) {
+	public static StaffData userCopyEntity(UserForm registform) {
 		StaffData entity = new StaffData();
 		entity.setStaffNo(registform.getStaffNo());
 		entity.setName(registform.getName());
@@ -68,18 +68,18 @@ public class BeanCopy {
 		entity.setAuthNo(registform.getAuth());
 		return entity;
 	}
-	
+
 	/**
 	 * ユーザー情報
 	 * entityからformにコピー
 	 */
-	public static UserRegistForm userCopyForm(StaffData staffData) {
-		 UserRegistForm form = new UserRegistForm();
-	        form.setOldStaffNo(staffData.getStaffNo());
-	        form.setStaffNo(staffData.getStaffNo());
-	        form.setName(staffData.getName());
-	        form.setPassword(staffData.getPassword());
-	        form.setAuth(staffData.getAuthNo());
-	        return form;
+	public static UserForm userCopyForm(StaffData staffData) {
+		UserForm form = new UserForm();
+		form.setOldStaffNo(staffData.getStaffNo());
+		form.setStaffNo(staffData.getStaffNo());
+		form.setName(staffData.getName());
+		form.setPassword(staffData.getPassword());
+		form.setAuth(staffData.getAuthNo());
+		return form;
 	}
 }
