8d729caddcd86383dd76afe32d2b96089cf3f465
komatsu1173 <yu85wave@gmail.com>
2026-03-26 10:00:10 +0900

    fix:UserUpdateMapper.java ユーザー情報の変更マッパーを追加

--- equipment/src/main/java/jp/co/sss/equipment/mapper/UserUpdateMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/UserUpdateMapper.java
@@ -2,10 +2,17 @@ package jp.co.sss.equipment.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import jp.co.sss.equipment.form.UserForm;
+
 /**
  * ユーザー情報編集マッパー
  */
 @Mapper
 public interface UserUpdateMapper {
+
+	/*
+	 * ユーザー情報の変更
+	 */
+	int userUpdate(UserForm updateForm);
 
 }
