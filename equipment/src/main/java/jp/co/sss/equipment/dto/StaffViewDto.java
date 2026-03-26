4c58f7b980d6ad01d2d1c6ec7e50f3f6cfded958
komatsu1173 <yu85wave@gmail.com>
2026-03-26 09:55:30 +0900

    add:StaffViewDto.java ユーザー情報をビューに表示させるDTOを作成

---
+++ equipment/src/main/java/jp/co/sss/equipment/dto/StaffViewDto.java
@@ -0,0 +1,23 @@
+package jp.co.sss.equipment.dto;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報をビューに表示する
+ */
+@Data
+public class StaffViewDto {
+	
+	/**スタッフID*/
+	private Integer staffNo;
+	
+	/**名前*/
+	private String name;
+	
+	/**権限id*/
+	private Integer authNo;
+	
+	/**権限名*/
+	private String authName;
+
+}
