8cccc0197062fa1bbfea7d22f5da8601246697ee
komatsu1173 <yu85wave@gmail.com>
2026-04-07 15:22:14 +0900

    add:EquipmentSearchDto.java 備品検索表示用DTOの作成

---
+++ equipment/src/main/java/jp/co/sss/equipment/dto/EquipmentSearchDto.java
@@ -0,0 +1,27 @@
+package jp.co.sss.equipment.dto;
+
+import lombok.Data;
+
+/**
+ * 備品検索表示DTO
+ */
+
+@Data
+public class EquipmentSearchDto {
+
+	/**シリアル*/
+    private String stockCode;
+    
+    /**備品名*/
+    private String name;
+    
+    /**カテゴリ*/
+    private String categoryName;
+    
+    /**貸出画面*/
+    private String statusName;
+    
+    /**リース分別*/
+    private String ownershipTypeName;
+    
+}
