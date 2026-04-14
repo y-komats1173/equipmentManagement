5464b3468ce1a0356c1a33181e0d3c5cd9297c74
komatsu1173 <yu85wave@gmail.com>
2026-04-14 10:31:59 +0900

    add:InventoryController.java 棚卸し用コントローラクラスを作成

---
+++ equipment/src/main/java/jp/co/sss/equipment/controller/InventoryController.java
@@ -0,0 +1,51 @@
+package jp.co.sss.equipment.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jp.co.sss.equipment.dto.EquipmentSearchDto;
+import jp.co.sss.equipment.form.EquipmentSearchForm;
+import jp.co.sss.equipment.service.EquipmentCommonService;
+import jp.co.sss.equipment.service.EquipmentSearchService;
+
+/**
+ * 棚卸しコントローラ
+ */
+
+@Controller
+public class InventoryController {
+	@Autowired
+	EquipmentCommonService equipmentCommonService;
+	
+	@Autowired
+	EquipmentSearchService equipmentSearchService;
+
+	/**
+	 * 棚卸し画面遷移
+	 */
+	@GetMapping("equipment/inventory")
+	public String inventoryView(@ModelAttribute("searchForm") EquipmentSearchForm form,
+	        Model model) {
+
+	    model.addAttribute("categoryList", equipmentCommonService.categoryFind());
+
+	    boolean hasCondition =
+	            (form.getStockCode() != null && !form.getStockCode().isBlank()) ||
+	            (form.getName() != null && !form.getName().isBlank()) ||
+	            form.getStockType() != null ||
+	            (form.getStatus() != null && !form.getStatus().isBlank()) ||
+	            form.getOwnershipType() != null;
+
+	    if (hasCondition) {
+	        List<EquipmentSearchDto> resultList = equipmentSearchService.search(form);
+	        model.addAttribute("resultList", resultList);
+	    }
+
+	    return "inventory/inventoryView";
+	}
+}
