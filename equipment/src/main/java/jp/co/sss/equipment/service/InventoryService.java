80c2d877a6bf0e8b41018bdf54cbba0eb2566e11
komatsu1173 <yu85wave@gmail.com>
2026-04-15 09:10:38 +0900

    fix:InventoryService.java 最終確認日の更新メソッドを実装

--- equipment/src/main/java/jp/co/sss/equipment/service/InventoryService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/InventoryService.java
@@ -1,6 +1,13 @@
 package jp.co.sss.equipment.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sss.equipment.dto.EquipmentSearchDto;
+import jp.co.sss.equipment.mapper.InventoryMapper;
 
 /**
  * 棚卸し用サービス
@@ -8,5 +15,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class InventoryService {
+	
+	@Autowired
+	InventoryMapper inventoryMapper;
 
+	/**
+	 * 最終確認日の更新
+	 * @param list
+	 */
+    @Transactional
+    public void inventoryUpdate(List<EquipmentSearchDto> list) {
+
+        for (EquipmentSearchDto dto : list) {
+
+            if (!dto.isChecked()) {
+                continue;
+            }
+            inventoryMapper.updateConfirmDate(dto.getStockCode());
+        }
+    }
 }
