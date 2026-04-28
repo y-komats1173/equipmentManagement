bcd903f00101911d00417cfdec6cb8445456d8c3
komatsu1173 <yu85wave@gmail.com>
2026-04-28 14:17:44 +0900

    feat:Enable scheduling and add random number generation on application startup

--- equipment/src/main/java/jp/co/sss/equipment/EquipmentApplication.java
+++ equipment/src/main/java/jp/co/sss/equipment/EquipmentApplication.java
@@ -1,13 +1,23 @@
 package jp.co.sss.equipment;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling 
 public class EquipmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EquipmentApplication.class, args);
+		System.out.println("システム起動");
+		
+		Random rand = new Random();
+		
+		int i = rand.nextInt(900000) + 100000;
+		System.out.println(i);
 	}
 
 }
