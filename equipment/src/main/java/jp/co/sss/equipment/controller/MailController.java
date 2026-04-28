9ca7ff5cb37c66a7aa3d68a0fc6b35178f59e1a8
komatsu1173 <yu85wave@gmail.com>
2026-04-28 14:19:15 +0900

    fix:MailController.java メールアドレスのプレースホルダーを追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/MailController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/MailController.java
@@ -1,7 +1,6 @@
 package jp.co.sss.equipment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -22,6 +21,7 @@ public class MailController {
 	@GetMapping("/mail/test")
 	public String sendTestMail() {	
 		mailService.sendMail(
+				"******@bg.co.jp",
 				"test@examle.com",
 				"テストメール",
 				"MailHog動作確認"
@@ -32,9 +32,9 @@ public class MailController {
 	/**
 	 * 返却日時がすぎた時
 	 */
-	@Scheduled
 	public void returnOver() {
 		mailService.sendMail(
+				"******@bg.co.jp",
 				"test@examle.com",
 				"履歴が開かれました",
 				"MailHog動作確認"
