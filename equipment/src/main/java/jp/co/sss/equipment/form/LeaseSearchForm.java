864df1cd191b4a3ace9424abfdab8276389d4b16
komatsu1173 <yu85wave@gmail.com>
2026-05-13 17:52:00 +0900

    add:LeaseSearchForm.java リース一覧表示用の条件検索フォームを追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/form/LeaseSearchForm.java
@@ -0,0 +1,16 @@
+package jp.co.sss.equipment.form;
+
+/**
+ * リース一覧表示　条件検索用
+ */
+public class LeaseSearchForm {
+
+	/**カテゴリ*/
+	private Integer categoryId;
+	
+	/**状態*/
+	private String status;
+	
+	/**並び順*/
+	private String sortType;
+}
